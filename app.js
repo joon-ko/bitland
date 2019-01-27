c406fbb03c9c1c93f0b117a8d7cf288744dd131c
Joonho Ko <joonhok@mit.edu>
2019-01-26 19:19:19 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(3000, () => {console.log('app listening on port 3000')});
